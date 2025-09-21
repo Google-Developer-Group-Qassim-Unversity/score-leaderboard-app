2813d08c26bed53478eb6b5d319b33510e1b54cf
Albrrak773 <albrrak773@gmail.com>
2025-09-21 20:17:26 +0300

    added google tag and vercel analytics

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -4,7 +4,7 @@ import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { Navigation } from "@/components/navigation"
 import { Suspense } from "react"
-
+import Script from 'next/script';
 export const metadata: Metadata = {
   title: "GDG",
 }
@@ -19,6 +19,18 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.png" type="image/png"/>
         <link rel="manifest" href="/manifest.json" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z62ENW3LFQ"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z62ENW3LFQ');
+          `}
+        </Script>
       </head>
       <body className="font-sans">
         <Suspense fallback={<div>Loading...</div>}>
